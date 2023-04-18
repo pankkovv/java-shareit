ef19923ac5439ca11616c60024f500f433390987
Панков Денис <Pankkovv@yandex.ru>
2023-04-18 17:36:31 +0300

    feat: create exception controller

--- src/main/java/ru/practicum/shareit/exception/controller/ErrorHandler.java
+++ src/main/java/ru/practicum/shareit/exception/controller/ErrorHandler.java
@@ -4,6 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import ru.practicum.shareit.exception.ConflictException;
 import ru.practicum.shareit.exception.NotFoundException;
 import ru.practicum.shareit.exception.model.ErrorResponse;
 
@@ -12,8 +13,14 @@ import ru.practicum.shareit.exception.model.ErrorResponse;
 public class ErrorHandler {
 
     @ExceptionHandler
-    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
     public ErrorResponse handleNotFoundException(final NotFoundException e) {
+        return new ErrorResponse(e.getMessage());
+    }
+
+    @ExceptionHandler
+    @ResponseStatus(HttpStatus.CONFLICT)
+    public ErrorResponse handleNotFoundException(final ConflictException e) {
         return new ErrorResponse(e.getMessage());
     }
 

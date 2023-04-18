9f95432d24eba0e4d0a6cd94b1acd3190e004efb
Панков Денис <Pankkovv@yandex.ru>
2023-04-18 17:38:41 +0300

    feat: create class mapper for objects app

--- src/main/java/ru/practicum/shareit/item/mapper/ItemMap.java
+++ src/main/java/ru/practicum/shareit/item/mapper/ItemMap.java
@@ -1,10 +1,23 @@
 package ru.practicum.shareit.item.mapper;
 
+import org.springframework.stereotype.Component;
 import ru.practicum.shareit.item.dto.ItemDto;
 import ru.practicum.shareit.item.model.Item;
 
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
 public class ItemMap {
-    public ItemDto transferObj(Item item){
-        return ItemDto.builder().id(item.getId()).name(item.getName()).description(item.getDescription()).available(item.isAvailable()).build();
+    public ItemDto transferObj(Item item) {
+        return ItemDto.builder().id(item.getId()).owner(item.getOwner()).name(item.getName()).description(item.getDescription()).available(Boolean.parseBoolean(item.getAvailable())).build();
+    }
+
+    public List<ItemDto> transferObj(List<Item> listItem) {
+        List<ItemDto> listItemDto = new ArrayList<>();
+        listItem.stream().forEach(item -> {
+            listItemDto.add(ItemDto.builder().id(item.getId()).owner(item.getOwner()).name(item.getName()).description(item.getDescription()).available(Boolean.parseBoolean(item.getAvailable())).build());
+        });
+        return listItemDto;
     }
 }
--- src/main/java/ru/practicum/shareit/user/mapper/UserMap.java
+++ src/main/java/ru/practicum/shareit/user/mapper/UserMap.java
@@ -1,10 +1,23 @@
 package ru.practicum.shareit.user.mapper;
 
+import org.springframework.stereotype.Component;
 import ru.practicum.shareit.user.dto.UserDto;
 import ru.practicum.shareit.user.model.User;
 
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
 public class UserMap {
-    public UserDto transferObj(User user){
-        return UserDto.builder().name(user.getName()).email(user.getEmail()).build();
+    public UserDto transferObj(User user) {
+        return UserDto.builder().id(user.getId()).name(user.getName()).email(user.getEmail()).build();
+    }
+
+    public List<UserDto> transferObj(List<User> listUser) {
+        List<UserDto> userDtoList = new ArrayList<>();
+        listUser.stream().forEach(user -> {
+            userDtoList.add(UserDto.builder().id(user.getId()).name(user.getName()).email(user.getEmail()).build());
+        });
+        return userDtoList;
     }
 }

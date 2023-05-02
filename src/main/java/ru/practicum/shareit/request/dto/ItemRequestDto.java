1cd74c7d4c39f9c0715e6419193721ee12e87183
Панков Денис <pankkovv@yandex.ru>
2023-05-02 16:37:07 +0300

    feat: create new object request

--- src/main/java/ru/practicum/shareit/request/dto/ItemRequestDto.java
+++ src/main/java/ru/practicum/shareit/request/dto/ItemRequestDto.java
@@ -1,7 +1,24 @@
 package ru.practicum.shareit.request.dto;
 
+import lombok.Builder;
+import lombok.Data;
+import ru.practicum.shareit.user.model.User;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
 /**
  * TODO Sprint add-item-requests.
  */
+@Data
+@Builder
 public class ItemRequestDto {
+    private Long id;
+    @NotBlank
+    @NotEmpty
+    private String description;
+    @NotNull
+    private Long requestor;
 }
---
+++ src/main/java/ru/practicum/shareit/request/mapper/ItemRequestMap.java
@@ -0,0 +1,35 @@
+package ru.practicum.shareit.request.mapper;
+
+import ru.practicum.shareit.request.dto.ItemRequestDto;
+import ru.practicum.shareit.request.model.ItemRequest;
+import ru.practicum.shareit.user.dto.UserDto;
+import ru.practicum.shareit.user.model.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ItemRequestMap {
+    public ItemRequest mapToItemRequest(ItemRequestDto itemRequestDto, User user) {
+        return ItemRequest.builder()
+                .id(itemRequestDto.getId())
+                .description(itemRequestDto.getDescription())
+                .requestor(user)
+                .build();
+    }
+
+    public ItemRequestDto mapToItemRequestDto(ItemRequest itemRequest) {
+        return ItemRequestDto.builder()
+                .id(itemRequest.getId())
+                .description(itemRequest.getDescription())
+                .requestor(itemRequest.getRequestor().getId())
+                .build();
+    }
+
+    public List<ItemRequestDto> mapToUserDto(List<ItemRequest> itemRequestList) {
+        List<ItemRequestDto> itemRequestDtoList = new ArrayList<>();
+        for (ItemRequest itemRequest : itemRequestList) {
+            itemRequestDtoList.add(mapToItemRequestDto(itemRequest));
+        }
+        return itemRequestDtoList;
+    }
+}

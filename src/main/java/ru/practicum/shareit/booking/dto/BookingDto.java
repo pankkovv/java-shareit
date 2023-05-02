968ad80d44605e7677805d5f4d5853652df2f625
Панков Денис <pankkovv@yandex.ru>
2023-05-02 16:34:37 +0300

    feat: create new model bookingDto

--- src/main/java/ru/practicum/shareit/booking/dto/BookingDto.java
+++ src/main/java/ru/practicum/shareit/booking/dto/BookingDto.java
@@ -1,7 +1,32 @@
 package ru.practicum.shareit.booking.dto;
 
+import lombok.Builder;
+import lombok.Data;
+import ru.practicum.shareit.booking.status.BookingStatus;
+import ru.practicum.shareit.item.model.Item;
+import ru.practicum.shareit.user.model.User;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+
 /**
  * TODO Sprint add-bookings.
  */
+@Data
+@Builder
 public class BookingDto {
+    private Long id;
+    @NotNull
+    private String start;
+    @NotNull
+    private String end;
+    @NotNull
+    private Long item;
+    @NotNull
+    private Long booker;
+    @NotBlank
+    @NotEmpty
+    private BookingStatus bookingStatus;
 }

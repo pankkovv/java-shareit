c1d56c5508597e89b4cfd8900cbdec95d10aa787
Панков Денис <Pankkovv@yandex.ru>
2023-05-11 15:38:39 +0300

    feat: create interface service for items

--- src/main/java/ru/practicum/shareit/item/service/ItemService.java
+++ src/main/java/ru/practicum/shareit/item/service/ItemService.java
@@ -2,21 +2,19 @@ package ru.practicum.shareit.item.service;
 
 import org.springframework.transaction.annotation.Transactional;
 import ru.practicum.shareit.item.dto.ItemDto;
-
 import ru.practicum.shareit.item.dto.ItemDtoWithBookingAndComments;
 
 import java.util.List;
 
 @Transactional
 public interface ItemService {
-
     List<ItemDtoWithBookingAndComments> getByUserId(Long userId);
 
-    public ItemDtoWithBookingAndComments getByItemId(Long userId, Long itemId);
+    ItemDtoWithBookingAndComments getByItemId(Long userId, Long itemId);
 
-    public List<ItemDto> search(String text);
+    List<ItemDto> search(String text);
 
-    public ItemDto saveItem(Long userId, ItemDto itemDto);
+    ItemDto saveItem(Long userId, ItemDto itemDto);
 
-    public ItemDto updateItem(Long userId, Long itemId, ItemDto itemDto);
+    ItemDto updateItem(Long userId, Long itemId, ItemDto itemDto);
 }

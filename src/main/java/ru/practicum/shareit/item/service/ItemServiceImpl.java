c16adeaa3543fbac5d4c35d4f1e434f659577c86
Панков Денис <Pankkovv@yandex.ru>
2023-05-11 15:39:08 +0300

    feat: create implements interface service for items

--- src/main/java/ru/practicum/shareit/item/service/ItemServiceImpl.java
+++ src/main/java/ru/practicum/shareit/item/service/ItemServiceImpl.java
@@ -2,10 +2,9 @@ package ru.practicum.shareit.item.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import ru.practicum.shareit.booking.dto.BookingDto;
+import org.springframework.transaction.annotation.Transactional;
 import ru.practicum.shareit.booking.dto.BookingWithDate;
 import ru.practicum.shareit.booking.mapper.BookingMap;
-import ru.practicum.shareit.booking.model.Booking;
 import ru.practicum.shareit.booking.repository.BookingRepository;
 import ru.practicum.shareit.comment.dto.CommentDto;
 import ru.practicum.shareit.comment.mapper.CommentMap;
@@ -17,14 +16,11 @@ import ru.practicum.shareit.item.dto.ItemDtoWithBookingAndComments;
 import ru.practicum.shareit.item.mapper.ItemMap;
 import ru.practicum.shareit.item.model.Item;
 import ru.practicum.shareit.item.repository.ItemRepository;
+import ru.practicum.shareit.messages.ExceptionMessages;
 import ru.practicum.shareit.user.model.User;
 import ru.practicum.shareit.user.repository.UserRepository;
 
-import org.springframework.transaction.annotation.Transactional;
-
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 
@@ -39,14 +35,13 @@ class ItemServiceImpl implements ItemService {
 
     @Override
     public List<ItemDtoWithBookingAndComments> getByUserId(Long userId) {
-        ArrayList<BookingWithDate> bookingsLast = new ArrayList<>();
-        ArrayList<BookingWithDate> bookingsNext = new ArrayList<>();
-
+        HashMap<Long, BookingWithDate> bookingsLast = new HashMap<>();
+        HashMap<Long, BookingWithDate> bookingsNext = new HashMap<>();
         HashMap<Long, List<CommentDto>> comments = new HashMap<>();
         List<Item> items = itemRepository.findByOwnerId(userId);
-        for(Item i : items){
-            bookingsLast.add(BookingMap.mapToBookingWithoutDate(bookingRepository.findByItemIdLast(userId, i.getId())));
-            bookingsNext.add(BookingMap.mapToBookingWithoutDate(bookingRepository.findByItemIdNext(userId, i.getId(), LocalDateTime.now())));
+        for (Item i : items) {
+            bookingsLast.put(i.getId(), BookingMap.mapToBookingWithoutDate(bookingRepository.findByItemIdLast(userId, i.getId(), LocalDateTime.now())));
+            bookingsNext.put(i.getId(), BookingMap.mapToBookingWithoutDate(bookingRepository.findByItemIdNext(userId, i.getId(), LocalDateTime.now())));
             comments.put(i.getId(), CommentMap.mapToCommentDto(commentRepository.findCommentById(i.getId())));
         }
         return ItemMap.mapToItemDtoWithBookingAndComments(items, bookingsLast, bookingsNext, comments);
@@ -54,17 +49,18 @@ class ItemServiceImpl implements ItemService {
 
     @Override
     public ItemDtoWithBookingAndComments getByItemId(Long userId, Long itemId) {
-        BookingWithDate bookingLast = BookingMap.mapToBookingWithoutDate(bookingRepository.findByItemIdLast(userId, itemId));
+        Item item = itemRepository.findById(itemId)
+                .orElseThrow(() -> new NotFoundException("Item not found."));
+        BookingWithDate bookingLast = BookingMap.mapToBookingWithoutDate(bookingRepository.findByItemIdLast(userId, itemId, LocalDateTime.now()));
         BookingWithDate bookingNext = BookingMap.mapToBookingWithoutDate(bookingRepository.findByItemIdNext(userId, itemId, LocalDateTime.now()));
         List<CommentDto> comments = CommentMap.mapToCommentDto(commentRepository.findCommentById(itemId));
-        Item item = itemRepository.findById(itemId).orElseThrow(() -> new NotFoundException("Item not found."));
         return ItemMap.mapToItemDtoWithBookingAndComments(item, bookingLast, bookingNext, comments);
     }
 
     @Override
     public List<ItemDto> search(String text) {
-        if(!text.isEmpty()){
-            return ItemMap.mapToItemDto(itemRepository.findByNameContainingIgnoreCaseOrDescriptionContainingIgnoreCaseAndAvailable(text, text, true));
+        if (!text.isEmpty()) {
+            return ItemMap.mapToItemDto(itemRepository.search(text));
         } else {
             return List.of();
         }
@@ -73,27 +69,31 @@ class ItemServiceImpl implements ItemService {
     @Override
     public ItemDto saveItem(Long userId, ItemDto itemDto) {
         User user = userRepository.findById(userId)
-                .orElseThrow(() -> new NotOwnerException("User not found."));
+                .orElseThrow(() -> new NotOwnerException(ExceptionMessages.NOT_FOUND_USER.label));
         return ItemMap.mapToItemDto(itemRepository.save(ItemMap.mapToItem(itemDto, user)));
     }
 
     @Override
     public ItemDto updateItem(Long userId, Long itemId, ItemDto itemDto) {
-        User user = userRepository.findById(userId)
-                .orElseThrow(() -> new NotOwnerException("User not found."));
+        validateExistUser(userId);
         ItemDto itemDtoOld = ItemMap.mapToItemWithBookingAndComments(getByItemId(userId, itemId));
-        if(itemDto.getName() != null){
+        if (itemDto.getName() != null) {
             itemDtoOld.setName(itemDto.getName());
         }
-        if(itemDto.getDescription() != null){
+        if (itemDto.getDescription() != null) {
             itemDtoOld.setDescription(itemDto.getDescription());
         }
-        if(itemDto.getAvailable() != null){
+        if (itemDto.getAvailable() != null) {
             itemDtoOld.setAvailable(itemDto.getAvailable());
         }
-        if(itemDto.getRequest() != null){
+        if (itemDto.getRequest() != null) {
             itemDtoOld.setRequest(itemDto.getRequest());
         }
         return saveItem(userId, itemDtoOld);
+    }
+
+    void validateExistUser(Long userId) {
+        userRepository.findById(userId)
+                .orElseThrow(() -> new NotOwnerException(ExceptionMessages.NOT_FOUND_USER.label));
     }
 }

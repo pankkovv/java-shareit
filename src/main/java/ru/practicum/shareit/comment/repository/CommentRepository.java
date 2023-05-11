8c6197adf4244d0175580b293d6dd91add5d95dc
Панков Денис <Pankkovv@yandex.ru>
2023-05-11 15:37:50 +0300

    feat: create interface repository Jpafor comments

--- src/main/java/ru/practicum/shareit/comment/repository/CommentRepository.java
+++ src/main/java/ru/practicum/shareit/comment/repository/CommentRepository.java
@@ -4,7 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import ru.practicum.shareit.comment.model.Comment;
 
-import javax.validation.Valid;
 import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {

f31c7bacf5f98349949ddb2c51a06f603a5f67c2
Панков Денис <pankkovv@yandex.ru>
2023-05-13 13:07:48 +0300

    feat: add annotation @Repository

--- src/main/java/ru/practicum/shareit/booking/repository/BookingRepository.java
+++ src/main/java/ru/practicum/shareit/booking/repository/BookingRepository.java
@@ -3,12 +3,14 @@ package ru.practicum.shareit.booking.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 import ru.practicum.shareit.booking.model.Booking;
 import ru.practicum.shareit.booking.status.BookingStatus;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
+@Repository
 public interface BookingRepository extends JpaRepository<Booking, Long>, CrudRepository<Booking, Long> {
     @Query(value = "select b.* from bookings as b " +
             "left join items as i on b.item_id = i.id " +
--- src/main/java/ru/practicum/shareit/comment/repository/CommentRepository.java
+++ src/main/java/ru/practicum/shareit/comment/repository/CommentRepository.java
@@ -2,10 +2,12 @@ package ru.practicum.shareit.comment.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 import ru.practicum.shareit.comment.model.Comment;
 
 import java.util.List;
 
+@Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
     @Query(value = "select c.* from comments as c " +

6b78df061f5c2028b7de448f5389de7b0db3268d
Панков Денис <pankkovv@yandex.ru>
2023-06-02 22:05:12 +0300

    backup saving

--- server/src/test/java/ru/practicum/server/booking/service/BookingServiceTest.java
+++ server/src/test/java/ru/practicum/server/booking/service/BookingServiceTest.java
@@ -2,6 +2,7 @@ package ru.practicum.server.booking.service;
 
 import lombok.RequiredArgsConstructor;
 import org.hamcrest.MatcherAssert;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -35,22 +36,39 @@ public class BookingServiceTest {
     private final UserService userService;
     private final ItemService itemService;
 
+    LocalDateTime start;
+    LocalDateTime end;
+    User user;
+    User owner;
+    UserDto userDto;
+    UserDto userOwnerDto;
+    Item item;
+    ItemDto itemDto;
+    BookingShort bookingShort;
+    BookingDto bookingDto;
+    UserDto userDtoDb;
+    UserDto ownerDtoDb;
+    ItemDto itemDtoDb;
+
+    @BeforeEach
+    void assistant() {
+        start = LocalDateTime.now();
+        end = LocalDateTime.now().plusMinutes(1);
+        user = User.builder().id(1L).name("User").email("user@user.ru").build();
+        owner = User.builder().id(2L).name("Owner").email("owner@user.ru").build();
+        userDto = UserDto.builder().id(1L).name("User").email("user@user.ru").build();
+        userOwnerDto = UserDto.builder().id(2L).name("Owner").email("owner@user.ru").build();
+        item = Item.builder().id(1L).owner(owner).name("Item").description("Item items").available(true).request(null).build();
+        itemDto = ItemDto.builder().name("Item").description("Item items").available(true).owner(owner.getId()).requestId(null).build();
+        bookingShort = BookingShort.builder().start(start).end(end).itemId(item.getId()).build();
+        bookingDto = BookingDto.builder().start(start).end(end).status(BookingStatus.WAITING).booker(user).item(item).build();
+        userDtoDb = userService.saveUser(userDto);
+        ownerDtoDb = userService.saveUser(userOwnerDto);
+        itemDtoDb = itemService.saveItem(ownerDtoDb.getId(), itemDto);
+    }
+
     @Test
     public void bookingAddTest() {
-        LocalDateTime start = LocalDateTime.now();
-        LocalDateTime end = LocalDateTime.now().plusMinutes(1);
-        User user = User.builder().id(1L).name("User").email("user@user.ru").build();
-        User owner = User.builder().id(2L).name("Owner").email("owner@user.ru").build();
-        UserDto userDto = UserDto.builder().id(1L).name("User").email("user@user.ru").build();
-        UserDto userOwnerDto = UserDto.builder().id(2L).name("Owner").email("owner@user.ru").build();
-        Item item = Item.builder().id(1L).owner(owner).name("Item").description("Item items").available(true).request(null).build();
-        ItemDto itemDto = ItemDto.builder().name("Item").description("Item items").available(true).owner(owner.getId()).requestId(null).build();
-        BookingShort bookingShort = BookingShort.builder().start(start).end(end).itemId(item.getId()).build();
-        BookingDto bookingDto = BookingDto.builder().start(start).end(end).status(BookingStatus.WAITING).booker(user).item(item).build();
-
-        userService.saveUser(userDto);
-        userService.saveUser(userOwnerDto);
-        itemService.saveItem(owner.getId(), itemDto);
         bookingService.bookingAdd(userDto.getId(), bookingShort);
 
         TypedQuery<Booking> query = em.createQuery("Select u from Booking u where u.booker.id = :bookerId", Booking.class);
@@ -66,19 +84,6 @@ public class BookingServiceTest {
 
     @Test
     public void bookingConfirmTest() {
-        LocalDateTime start = LocalDateTime.now();
-        LocalDateTime end = LocalDateTime.now().plusMinutes(1);
-        User user = User.builder().id(3L).name("User").email("user@user.ru").build();
-        User owner = User.builder().id(4L).name("Owner").email("owner@user.ru").build();
-        UserDto userDto = UserDto.builder().id(1L).name("User").email("user@user.ru").build();
-        UserDto userOwnerDto = UserDto.builder().id(4L).name("Owner").email("owner@user.ru").build();
-        Item item = Item.builder().id(2L).owner(owner).name("Item").description("Item items").available(true).request(null).build();
-        ItemDto itemDto = ItemDto.builder().name("Item").description("Item items").available(true).owner(owner.getId()).requestId(null).build();
-        BookingDto bookingDto = BookingDto.builder().start(start).end(end).status(BookingStatus.WAITING).booker(user).item(item).build();
-
-        UserDto userDtoDb = userService.saveUser(userDto);
-        UserDto ownerDtoDb = userService.saveUser(userOwnerDto);
-        ItemDto itemDtoDb = itemService.saveItem(ownerDtoDb.getId(), itemDto);
         BookingShort bookingShort = BookingShort.builder().start(start).end(end).itemId(itemDtoDb.getId()).build();
         BookingDto bookingDtoDb = bookingService.bookingAdd(userDtoDb.getId(), bookingShort);
         bookingService.bookingConfirm(ownerDtoDb.getId(), bookingDtoDb.getId(), true);
@@ -89,8 +94,6 @@ public class BookingServiceTest {
         MatcherAssert.assertThat(booking.getId(), notNullValue());
         MatcherAssert.assertThat(booking.getStart(), equalTo(bookingDto.getStart()));
         MatcherAssert.assertThat(booking.getEnd(), equalTo(bookingDto.getEnd()));
-        MatcherAssert.assertThat(booking.getItem(), equalTo(bookingDto.getItem()));
-        MatcherAssert.assertThat(booking.getBooker(), equalTo(bookingDto.getBooker()));
         MatcherAssert.assertThat(booking.getBookingStatus(), equalTo(BookingStatus.APPROVED));
     }
 }
